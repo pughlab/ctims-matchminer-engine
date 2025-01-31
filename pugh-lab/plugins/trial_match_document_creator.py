9303426f569c0c72c6806e58740e58c0e718ebca
Dheeraj Gavini <dheerajgavini@Dheerajs-MacBook-Pro.local>
2025-01-31 12:26:52 -0500

    Changed match type from prior_treatment_agent to prior_treatment when there is match in prior treatment

--- pugh-lab/plugins/trial_match_document_creator.py
+++ pugh-lab/plugins/trial_match_document_creator.py
@@ -362,11 +362,11 @@ class PughLabTrialMatchDocumentCreator(TrialMatchDocumentCreator):
         agent = treatment_doc.get("AGENT")
         if agent:
             return (
-                "prior_treatment_agent",
+                "prior_treatment",
                 f"{agent}",
             )
         else:
-            return 'prior_treatment_agent', "prior_treatment_agent"
+            return 'prior_treatment', "prior_treatment_agent"
 
     def _format_prior_treatment_exclusion_match(self, match_reason: MatchReason):
         """
@@ -377,7 +377,7 @@ class PughLabTrialMatchDocumentCreator(TrialMatchDocumentCreator):
         if agent is None:
             agent = query.get('agent')
         return (
-            "prior_treatment_agent",
+            "prior_treatment",
             f"{agent}"
         )
 
